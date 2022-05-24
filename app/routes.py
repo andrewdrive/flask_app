c3d59b993ad1bd9fd701568d018bc2fb01a02158
andrewdrive <blank@mail.com>
2022-05-24 16:10:50 +0300

    in2

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return '<h1>Pong!<h1>'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+
+
